2d031aa0f0bd49739c2c40afe32aa34e0bc5a8b3
Marat Gabitov <gabitovmt@yandex.ru>
2025-03-11 00:38:07 +0500

    Изучаем Python. Глава 5. Дроби

---
+++ src/learning_python/ch05/script12 fraction.py
@@ -0,0 +1,45 @@
+# Дробный тип
+
+from fractions import Fraction
+
+from unicodedata import decimal
+
+x = Fraction(1, 3)  # Числитель, знаменатель
+y = Fraction(4, 6)  # Упрощается до 2, 3 по наибольшему общему делителю
+print(repr(x))  # Fraction(1, 3)
+print(repr(y))  # Fraction(2, 3)
+print(y)  # 2/3
+
+print(x + y)  # 1
+print(x - y)  # -1/3
+print(x * y)  # 2/9
+
+print(repr(Fraction('.25')))  # Fraction(1, 4)
+print(repr(Fraction('1.25')))  # Fraction(5, 4)
+print(repr(Fraction('.25') + Fraction('1.25')))  # Fraction(3, 2)
+
+print('\nЧисловая точность дробных и десятичных типов')
+a = 1 / 3.0  # Результат точен лишь настолько, насколько позволяют аппаратные средства
+b = 4 / 6.0  # Точность может теряться из-за множества вычислений
+print(a)  # 0.3333333333333333
+print(b)  # 0.6666666666666666
+print(a + b)  # 1.0
+print(a - b)  # -0.3333333333333333
+print(a * b)  # 0.2222222222222222
+
+print(0.1 + 0.1 + 0.1 - 0.3)  # Должен быть нулём (близко, но не точно)
+# 5.551115123125783e-17
+print(repr(Fraction(1, 10) + Fraction(1, 10) + Fraction(1, 10) - Fraction(3, 10)))
+# Fraction(0, 1)
+
+import decimal
+from decimal import Decimal
+
+print('\nЧисловая точность, два пути')
+print(1 / 3)
+# 0.3333333333333333
+print(Fraction(1, 3))
+# 1/3
+decimal.getcontext().prec = 2
+print(Decimal(1) / Decimal(3))
+# 0.33

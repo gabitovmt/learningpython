3a82d71594ebdbb8c0e650316ee1f12ce7d24c01
Marat Gabitov <gabitovmt@yandex.ru>
2025-03-10 23:55:57 +0500

    Изучаем Python. Глава 5. Десятичные типы

---
+++ src/learning_python/ch05/script11 decimal.py
@@ -0,0 +1,40 @@
+# Основы десятичных чисел
+from unicodedata import decimal
+
+print(0.1 + 0.1 + 0.1 - 0.3)
+# 5.551115123125783e-17: недостаточно битов, чтобы обеспечить точность
+
+from decimal import Decimal
+
+print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
+# 0.0
+
+# Python автоматически выполняет преобразование точности к наибольшему кол-ву десятичных цифр после точки
+print(Decimal('0.1') + Decimal('0.10') + Decimal('0.10') - Decimal('0.30'))
+# 0.00
+
+# float -> Decimal
+print(Decimal.from_float(1.25))
+print(Decimal(1.25))
+
+print(Decimal(0.1) + Decimal(0.1) + Decimal(0.1) - Decimal(0.3))
+# 2.775557561565156540423631668E-17
+
+print(Decimal(1) / Decimal(7))
+# 0.1428571428571428571428571429
+
+import decimal
+
+decimal.getcontext().prec = 4
+print(Decimal(1) / Decimal(7))
+# 0.1429
+
+print(Decimal(0.1) + Decimal(0.1) + Decimal(0.1) - Decimal(0.3))
+# 1.110E-17
+
+print('\nДиспетчер контекста для десятичных чисел')
+print(Decimal('1.00') / Decimal('3.00'))  # 0.3333
+with decimal.localcontext() as ctx:
+    ctx.prec = 2
+    print(Decimal('1.00') / Decimal('3.00'))  # 0.33
+print(Decimal('1.00') / Decimal('3.00'))  # 0.3333

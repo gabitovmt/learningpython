c124181c93f295d0247df9c6a155a094c28ff201
Marat Gabitov <gabitovmt@yandex.ru>
2025-05-28 09:44:27 +0500

    Булевские значения

---
+++ src/learning_python/ch05/script17 bool.py
@@ -0,0 +1,14 @@
+# Булевские значения
+
+print(type(True))
+# <class 'bool'>
+print(isinstance(True, int))
+# True
+print(True == 1) # То же самое значение
+# True
+print(True is 1) # Но другой объект
+# False
+print(True or False) # То же что и 1 or 0
+# True
+print(True + 4)
+# 5

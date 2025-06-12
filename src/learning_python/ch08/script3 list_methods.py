ec30d06eed47a7d91bc3e5a3244dd3fad6d94b2d
Marat Gabitov <gabitovmt@yandex.ru>
2025-06-13 02:00:10 +0500

    Методы списка

---
+++ src/learning_python/ch08/script3 list_methods.py
@@ -0,0 +1,36 @@
+L = [1, 2]
+L.extend([3, 4, 5])  # Добавление в конец списка
+print(L)
+# [1, 2, 3, 4, 5]
+
+print(L.pop())  # Удаление и возврат последнего элемента (по умолчанию: -1)
+# 5
+
+L.reverse()  # Метод обращения списка на месте
+print(L)
+# [4, 3, 2, 1]
+print(list(reversed(L)))  # Встроенная функция обращения списка с результатом (итератор)
+# [1, 2, 3, 4]
+
+L = [1, 2, 3]
+print(L.pop(1))
+# 2
+print(L)
+# [1, 3]
+
+print('\nЕщё методы')
+L = ['spam', 'eggs', 'ham']
+print(L.index('eggs'))  # Индекс объекта (поиск)
+# 1
+L.insert(1, 'toast')  # Вставка в позицию
+print(L)
+# ['spam', 'toast', 'eggs', 'ham']
+L.remove('eggs')
+print(L)
+# ['spam', 'toast', 'ham']
+print(L.pop(1))  # Удаление по позиции
+# toast
+print(L)
+# ['spam', 'ham']
+print(L.count('spam'))  # Кол-во вхождений
+# 1

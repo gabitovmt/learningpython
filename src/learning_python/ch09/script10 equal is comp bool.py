1bfaa7a0fec6a72f2b37f896209511a189b69ad0
Marat Gabitov <gabitovmt@yandex.ru>
2025-06-18 21:55:53 +0500

    Сравнения, тип bool

--- src/learning_python/ch09/script10 equal is comp bool.py
+++ src/learning_python/ch09/script10 equal is comp bool.py
@@ -17,3 +17,15 @@ print(S1 == S2, S1 is S2)
 L1 = [1, ('a', 3)]
 L2 = [1, ('a', 2)]
 print(L1 < L2, L1 == L2, L1 > L2)
+
+D1 = {'a': 1, 'b': 2}
+D2 = {'a': 1, 'b': 3}
+# print(D1 < D2, D1 > D2) TypeError
+
+print('\nТип bool')
+print(bool(1))
+# True
+print(bool('spam'))
+# True
+print(bool({}))
+# False

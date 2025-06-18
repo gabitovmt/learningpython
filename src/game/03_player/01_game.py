2fac24d3ac1ab9cd1e8f5cf3fc4071830ce3813d
Marat Gabitov <gabitovmt@yandex.ru>
2025-06-19 00:40:18 +0500

    Игровой персонаж

---
+++ src/game/03_player/01_game.py
@@ -0,0 +1,65 @@
+import pygame
+import sys
+
+"""
+Переменные
+"""
+
+worldx = 960
+worldy = 720
+fps = 40
+ani = 4  # Циклы анимации
+main = True
+
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+
+"""
+Объекты
+"""
+
+class Player(pygame.sprite.Sprite):
+    """
+    Создание главного персонажа
+    """
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        for i in range(0, 8):
+            img = pygame.image.load(f'images/walk-{i}.png').convert()
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+
+
+"""
+Настройка
+"""
+
+clock = pygame.time.Clock()
+pygame.init()
+
+world = pygame.display.set_mode((worldx, worldy))
+
+player = Player()
+player.rect.x = 0
+player.rect.y = 0
+player_list = pygame.sprite.Group()
+player_list.add(player)
+
+"""
+Главный цикл
+"""
+
+while main:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == ord('q')):
+            pygame.quit()
+            sys.exit()
+
+    world.fill(BLUE)
+    player_list.draw(world)
+    pygame.display.flip()
+    clock.tick(fps)

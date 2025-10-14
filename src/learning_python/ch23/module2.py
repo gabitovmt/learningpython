4222488b8fdc4e82b5448be2b7472a4c6d89db73
Gabitov <gabitov@parma.ru>
2025-10-14 14:32:12 +0500

    Глава 23. Основы написания модулей Пространства имён

---
+++ src/learning_python/ch23/module2.py
@@ -0,0 +1,6 @@
+print('starting to load ... ')  # Начало загрузки
+import sys
+name = 42
+def func(): pass
+class klass: pass
+print('done loading.')  # Загрузка окончена
---
+++ src/learning_python/ch23/script06 namespace.py
@@ -0,0 +1,12 @@
+import module2
+# starting to load ...
+# done loading.
+
+print(module2.sys)
+# <module 'sys' (built-in)>
+print(module2.name)
+# 42
+print(module2.func)
+# <function func at 0x000002BCC76B0B80>
+print(module2.klass)
+# <class 'module2.klass'>

da01efa7c130fc583eca68437d547bbd22fcebe3
Gabitov <gabitov@parma.ru>
2025-10-20 11:24:30 +0500

    Глава 24. Пакеты модулей Операции относительного импортирования в действии

---
+++ src/learning_python/ch24/script02/script02.py
@@ -0,0 +1,10 @@
+import string
+print(string)
+# <module 'string' from 'C:\\devprograms\\python\\Python313\\Lib\\string.py'>
+
+import sys
+print(sys.path)
+# ['C:\\Projects\\MyProjects\\learningpython\\src\\learning_python\\ch24\\script02', 'C:\\Projects\\MyProjects\\learningpython', ...]
+
+# from . import string
+# ImportError: attempted relative import with no known parent package

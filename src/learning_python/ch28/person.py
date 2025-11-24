be7a397def64a500dd86c967c0a16df8b17b6492
Marat Gabitov <gabitovmt@yandex.ru>
2025-11-24 21:31:42 +0500

    Глава 28. Более реалистичный пример

---
+++ src/learning_python/ch28/person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100_000)
+
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
